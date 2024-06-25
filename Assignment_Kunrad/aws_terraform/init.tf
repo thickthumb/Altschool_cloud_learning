97f4f998d393d98a8ad2df4255f6c7a994ec0668
thickthumb <davetaiwo59@gmail.com>
2024-06-25 19:11:26 +0000

    modules upload

--- Assignment_Kunrad/aws_terraform/init.tf
+++ Assignment_Kunrad/aws_terraform/init.tf
@@ -13,26 +13,3 @@ provider "aws" {
 }
 
 
-
-module "apigateway-v2" {
-  source  = "terraform-aws-modules/apigateway-v2/aws"
-  version = "5.0.0"
-}
-
-module "s3-bucket" {
-  source  = "cloudposse/s3-bucket/aws"
-  version = "4.2.0"
-}
-
-module "cloudfront" {
-  source  = "terraform-aws-modules/cloudfront/aws"
-  version = "3.4.0"
-}
-
-
-
-module "route53" {
-  source  = "mineiros-io/route53/aws"
-  version = "0.6.0"
-  # insert the 4 required variables here
-}
